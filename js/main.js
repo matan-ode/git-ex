7ca882b24c882ca1b4cc7be896ecd3d7cf74f385
matan-ode <matanodentz@gmail.com>
2024-09-10 16:47:10 +0300

    Add limit grow in onClickBall

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var diameter = 100
 
 function onBallClick(elBall){
     diameter += 50
+    if(diameter > 400) diameter = 100
+
     elBall.style.width = diameter + 'px'
     elBall.style.height = diameter + 'px'
     elBall.innerText = diameter
