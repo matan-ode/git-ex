8a2c4fc9540d7d8fdff32377c6b83262daa811c8
matan-ode <matanodentz@gmail.com>
2024-09-10 18:20:08 +0300

    Add changing size inner text to onBallClick3

--- js/main.js
+++ js/main.js
@@ -19,17 +19,25 @@ function onBallClick3() {
     const ballStyle1 = getComputedStyle(elBall1)
     const ballColor1 = ballStyle1.backgroundColor
     const ballSize1 = ballStyle1.width
+    const num1 = elBall1.innerText
 
     const elBall2 = document.querySelector('.ball2')
     const ballStyle2 = getComputedStyle(elBall2)
     const ballColor2 = ballStyle2.backgroundColor
     const ballSize2 = ballStyle2.width
+    const num2 = elBall2.innerText
+
 
     elBall1.style.backgroundColor = ballColor2
     elBall2.style.backgroundColor = ballColor1
 
     elBall1.style.width = ballSize2
-    elBall1.style.height = ballSize2
     elBall2.style.width = ballSize1
+    
+    elBall1.style.height = ballSize2
     elBall2.style.height = ballSize1
+
+    elBall1.innerText = num2
+    elBall2.innerText = num1
+
 }
