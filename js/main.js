86330d46832a7cbfd555e71ead6a825d88ba9aa3
matan-ode <matanodentz@gmail.com>
2024-09-10 18:16:05 +0300

    Update onBallClick3 style of balls

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 'use strict'
 var diameter = 100
+// const ball1Color = 'yellow'
+// const ball2Color = 'green' 
 
-function onBallClick(elBall, maxDiameter){
+function onBallClick(elBall, maxDiameter) {
     diameter += getRandomInt(20, 61)
-    if(diameter > maxDiameter) diameter = 100
+    if (diameter > maxDiameter) diameter = 100
 
     elBall.style.width = diameter + 'px'
     elBall.style.height = diameter + 'px'
@@ -12,9 +14,22 @@ function onBallClick(elBall, maxDiameter){
     elBall.style.backgroundColor = getRandomColor()
 }
 
-function onBallClick3(){
+function onBallClick3() {
     const elBall1 = document.querySelector('.ball')
-    onBallClick(elBall1, 200)
+    const ballStyle1 = getComputedStyle(elBall1)
+    const ballColor1 = ballStyle1.backgroundColor
+    const ballSize1 = ballStyle1.width
+
     const elBall2 = document.querySelector('.ball2')
-    onBallClick(elBall2, 500)
+    const ballStyle2 = getComputedStyle(elBall2)
+    const ballColor2 = ballStyle2.backgroundColor
+    const ballSize2 = ballStyle2.width
+
+    elBall1.style.backgroundColor = ballColor2
+    elBall2.style.backgroundColor = ballColor1
+
+    elBall1.style.width = ballSize2
+    elBall1.style.height = ballSize2
+    elBall2.style.width = ballSize1
+    elBall2.style.height = ballSize1
 }
