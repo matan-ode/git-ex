29f81a3b5d67a3b8135be2285955f02e4678b078
matan-ode <matanodentz@gmail.com>
2024-09-10 18:59:51 +0300

    Add ball4 and onBallClick4

--- js/main.js
+++ js/main.js
@@ -27,17 +27,40 @@ function onBallClick3() {
     const ballSize2 = ballStyle2.width
     const num2 = elBall2.innerText
 
-
     elBall1.style.backgroundColor = ballColor2
     elBall2.style.backgroundColor = ballColor1
 
     elBall1.style.width = ballSize2
     elBall2.style.width = ballSize1
-    
+
     elBall1.style.height = ballSize2
     elBall2.style.height = ballSize1
 
     elBall1.innerText = num2
     elBall2.innerText = num1
+}
+
+function onBallClick4() {
+    const numReduce = getRandomInt(20, 61)
+
+    const elBall1 = document.querySelector('.ball')
+    const elBall2 = document.querySelector('.ball2')
+
+    const ballSize1 = ballStyle1.width
+    const num1 = elBall1.innerText
+    const newSize1 = ballSize1 - numReduce
+
+    const ballSize2 = ballStyle2.width
+    const num2 = elBall2.innerText
+    const newSize2 = ballSize2 - numReduce
+
+    elBall1.style.width = newSize1
+    elBall2.style.width = newSize2
+
+    elBall1.style.height = newSize1
+    elBall2.style.height = newSize2
+
+    elBall1.innerText = newSize1
+    elBall2.innerText = newSize2
 
 }
